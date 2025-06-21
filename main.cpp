ad709d80061f1c864efaf8bd77b274f9cf286831
blinkgao <blinkgao@techtrex.com>
2025-06-22 00:48:49 +0800

    add remark

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Blink
+ * @Date: 2025-06-22 00:48:04
+ * @LastEditors: Blink
+ * @LastEditTime: 2025-06-22 00:48:13
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
